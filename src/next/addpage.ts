0002b0ecc97f3a0f95bb9e15cf278ec92a552d41
taoqf <tao_qiufeng@126.com>
2021-04-16 12:15:54 +0800

    add favicon

--- src/next/addpage.ts
+++ src/next/addpage.ts
@@ -248,6 +248,9 @@ const page: NextPage<IProps> = () => {
 		<>
 			<Head>
 				<title>${title || '01factory'}</title>
+				<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon"></link>
+				<link rel="icon" type="image/x-icon" sizes="32x32" href="/favicon-32x32.ico" ></link>
+				<link rel="icon" type="image/x-icon" sizes="16x16" href="/favicon-16x16.ico"></link>
 			</Head>
 			${cs.join('\n\t\t\t')}
 		</>
