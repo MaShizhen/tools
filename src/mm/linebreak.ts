63f3073d9a9cd4e2ddb489ef696cce4407e979db
taoqf <tao_qiufeng@126.com>
2021-05-01 11:35:12 +0800

    get select text

--- src/mm/linebreak.ts
+++ src/mm/linebreak.ts
@@ -2,7 +2,7 @@ import { env, TextEditor, window } from 'vscode';
 
 export default async function linebreak(editor: TextEditor) {
 	const sel = editor.selection;
-	const text = editor.document.getText();
+	const text = editor.document.getText(sel);
 	const d = text.replaceAll('\n\n', '\\n\n');
 	await env.clipboard.writeText(d);
 	await editor.edit((eb) => {
