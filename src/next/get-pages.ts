cbab61f6bc721b570e990d467fa4f1a4ea87ffbf
taoqf <tao_qiufeng@126.com>
2021-05-27 14:19:35 +0800

    fix: add service in nextjs

--- src/next/get-pages.ts
+++ src/next/get-pages.ts
@@ -3,11 +3,11 @@ import { promises as fs } from 'fs';
 
 export async function get_pages(rootPath: string) {
 	const pages = join(rootPath, 'pages');
-	if (await existsasync(join(pages, '_app.tsx'))) {
+	if (await existsasync(join(pages, '_app.page.tsx'))) {
 		return pages;
 	}
 	const src = join(rootPath, 'src', 'pages');
-	if (await existsasync(join(src, '_app.tsx'))) {
+	if (await existsasync(join(src, '_app.page.tsx'))) {
 		return src;
 	}
 	throw new Error('Wrong type');
