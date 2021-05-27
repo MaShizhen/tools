36709f72af5f16601cec09b558bde87fb6f57b7b
taoqf <tao_qiufeng@126.com>
2021-05-27 14:34:55 +0800

    next add service

--- src/next/addservice.ts
+++ src/next/addservice.ts
@@ -148,7 +148,8 @@ export default handler;
 		const curfile = editor.document.fileName;
 		const curname = basename(curfile);
 		const api = join(pages, 'api');
-		if (/^(\[.+\]|index.page)\.[tj]sx?$/.test(curname)) {
+		debugger;
+		if (/^(\[.+\]|index)\.page\.[tj]sx?$/.test(curname)) {
 			// absolutedir/src/pages/xxx/[yyy].tsx or absolutedir/src/pages/xxx/index.page.tsx
 			// absolutedir/src/pages/xxx
 			const relativepath = this.getrelativepath(pages, curdir);
