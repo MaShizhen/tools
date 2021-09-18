51dcafe9ed81768788d83bca3a2c00187fa7481e
taoqf <tao_qiufeng@126.com>
2021-09-18 18:09:20 +0800

    tpl

--- src/taro/addcomponent.ts
+++ src/taro/addcomponent.ts
@@ -1,1 +1,57 @@
-export { default } from '../next/addcomponent';
+import { basename, join } from 'path';
+import { Position, TextEditor, window } from 'vscode';
+import Actor from '../actor';
+
+export default class AddComponentTaro extends Actor {
+	public async do(editor: TextEditor): Promise<void> {
+		const dir = await this.getcurpath(editor.document.fileName, join(this.root(), 'src', 'components'));
+		const doc = editor.document;
+		const name = await this.generate(dir, 'c', 3);
+		const f = await window.showInputBox({
+			prompt: 'type path',
+			placeHolder: 'component path',
+			value: join(dir, name)
+		});
+		if (!f) {
+			return;
+		}
+		const filepath = f.toLowerCase();
+		const fullpath = `${filepath}.tsx`;
+		const cname = this.str2name(basename(filepath));
+		const content = await (async () => {
+			if (/\.tsx/.test(doc.fileName)) {
+				const sel = editor.selection;
+				const content = sel.isEmpty ? '' : doc.getText(sel);
+				const max = doc.lineCount;
+				let pos = -1;
+				for (let i = 0; i < max; i++) {
+					const line = doc.lineAt(i);
+					const text = line.text;
+					if (/^import\s+.+/.test(text)) {
+						pos = i;
+					}
+				}
+				const imppath = this.getrelativepath(dir, fullpath.replace(/\.tsx/, ''));
+				const imp = `import ${cname} from './${imppath}';\n`;
+				const imppos1 = new Position(pos + 1, 0);
+				await editor.edit((eb) => {
+					eb.replace(sel, `<${cname} />`);
+					eb.insert(imppos1, imp);
+				});
+				return content;
+			}
+			return '';
+		})();
+		const tpl = `import React from 'react';
+import { View, Text } from '@tarojs/components';
+export default function ${cname}() {
+	return <View>
+		<Text>${content}</Text>
+	</View>;
+}
+`;
+		await this.writefile(fullpath, tpl);
+		this.set_status_bar_message('成功添加组件');
+		await this.show_doc(fullpath);
+	}
+}
--- src/taro/addcomponent2.ts
+++ src/taro/addcomponent2.ts
@@ -1,1 +1,36 @@
-export { default } from '../next/addcomponent2';
+import { basename, join } from 'path';
+import { window } from 'vscode';
+import Actor from '../actor';
+
+export default class AddComponentTaro2 extends Actor {
+	public async do(d?: string): Promise<void> {
+		const dir = await this.getcurpath(d, join(this.root(), 'src', 'components'));
+		if (!dir) {
+			this.showerror('不能在当前页面插入组件');
+			return;
+		}
+		const name = await this.generate(dir, 'c', 3);
+		const f = await window.showInputBox({
+			prompt: 'type path',
+			placeHolder: 'component path',
+			value: join(dir, name)
+		});
+		if (!f) {
+			return;
+		}
+		const filepath = f.toLowerCase();
+		const fullpath = `${filepath}.tsx`;
+		const cname = this.str2name(basename(filepath));
+		const tpl = `import React from 'react';
+import { View, Text } from '@tarojs/components';
+export default function ${cname}() {
+	return <View>
+		<Text></Text>
+	</View>;
+}
+`;
+		await this.writefile(fullpath, tpl);
+		this.set_status_bar_message('成功添加组件');
+		await this.show_doc(fullpath);
+	}
+}
