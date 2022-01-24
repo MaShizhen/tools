00e19e953875cd3552edd7ce2c7dc446d5bf6198
taoqf <tao_qiufeng@126.com>
2022-01-24 19:51:24 +0800

    s

--- src/next/addservice.ts
+++ src/next/addservice.ts
@@ -1,7 +1,7 @@
 import { basename, extname, join } from 'path';
 import { commands, Position, window, workspace, WorkspaceEdit } from 'vscode';
-import { get_pages } from './get-pages';
 import Actor from '../actor';
+import { get_pages } from './get-pages';
 
 export default class AddServiceNext extends Actor {
 	public async do(p?: string): Promise<void> {
@@ -26,65 +26,70 @@ export default class AddServiceNext extends Actor {
 		// create service file
 		const servicefile = `${sfilepath}.api.ts`;
 		await this.create_api(servicefile);
+		await this.create_api_caller(sfilepath);
 		await commands.executeCommand('mm.regenerateapis');
 		if (page) {
 			// update page file
-			await this.updatepage(page, servicefile);
+			await this.updatepage(page, sfilepath);
 		}
 		await this.save();
 		this.set_status_bar_message('成功添加服务文件');
-		await this.show_doc(servicefile);
+		await this.show_doc(page || servicefile);
 	}
-
+	
 	private async updatepage(pagefile: string, servicefile: string) {
 		// api/xxx/yyy/s001
-		const imp = new RegExp('import\\s+api\\s+from\\s+[\'|"].*[\'|"];?');
 		const doc = await workspace.openTextDocument(pagefile);
-		const max = doc.lineCount;
-		let hasimport = false;
 		let pos = -1;
-		for (let i = 0; i < max; i++) {
+		for (let i = 0; i < doc.lineCount; i++) {
 			const line = doc.lineAt(i);
 			const text = line.text;
 			if (/^import\s+.+/.test(text)) {
-				if (imp.test(text)) {
-					hasimport = true;
-					break;
-				}
 				pos = i;
 			}
 		}
 		const we = new WorkspaceEdit();
-		if (!hasimport) {
-			const root = this.root();
-			const api = join(root, 'src', 'atoms', 'api');
-			const relativepath = this.getrelativepath(join(pagefile, '..'), api);
-			const imppath = relativepath.startsWith('.') ? relativepath : `./${relativepath}`;
-			const imp = `import api from '${imppath}';`;
-			const uri = doc.uri;
-			const imppos = new Position(pos + 1, 0);
-			we.insert(uri, imppos, `${imp}\n`);
-		}
-		{
-			const ext = '.ts';
-			const pathwithoutext = servicefile.replace(ext, '');
-			const relativepath = this.getrelativepath(join(pagefile, '..'), pathwithoutext);
-			const imppath = relativepath.startsWith('.') ? relativepath : `./${relativepath}`;
-			const body = doc.getText();
-			const ss = body.match(/[MQR]\d+/g) || ['0'];
-			const max = ss.map((s) => {
-				return Number(s.replace(/[^\d]/g, ''));
-			}).sort((a, b) => {
-				return b - a;
-			})[0];
-			const no = max + 1;
-			const imp = `import { Message as M${no}, Result as R${no} } from '${imppath}';`;
-			// const imp = `import { Message as M${no}, Query as Q${no}, Result as R${no} } from '${imppath}';`;
-			const uri = doc.uri;
-			const imppos = new Position(pos + 1, 0);
-			we.insert(uri, imppos, `${imp}\n`);
-		}
+		const pathwithoutext = servicefile;
+		const relativepath = this.getrelativepath(join(pagefile, '..'), pathwithoutext);
+		const imppath = relativepath.startsWith('.') ? relativepath : `./${relativepath}`;
+		const name = this.getimportname( this.str2camelcase(basename(pathwithoutext)), doc.getText());
+		
+		const imp = `import ${name} from '${imppath}';`;
+		const uri = doc.uri;
+		const imppos = new Position(pos + 1, 0);
+		we.insert(uri, imppos, `${imp}\n`);
 		await workspace.applyEdit(we);
+	}
+	
+	private getimportname(name: string, body:string) : string {
+		const name1 = `function ${name}`;
+		if (body.includes(name1)) {
+			return this.getimportname(`${name}_1`, body);
+		}
+		const name2 = `import ${name}`;
+		if (body.includes(name2)) {
+			return this.getimportname(`${name}_1`, body);
+		}
+		return name;
+	}
+
+	private create_api_caller(path: string) {
+		const filename = basename(path, '.ts');
+		const relativepath = this.getrelativepath(join( path, '../'), 'src/atoms/api');
+		const api = this.getrelativepath('src/pages',path);
+		const tpl = `import smartfetch from '@mmstudio/an000058';
+import api from '${relativepath}';
+import { Message, Result } from './${filename}.api';
+
+export default async function ${this.str2camelcase(filename)}(msg: Message) {
+	const ret = await smartfetch<Result, Message>(api['/${api}'], 'post', msg);
+	if(ret.ok === true){
+		return ret.data;
+	}
+	throw new Error(ret.message);
+}
+`;
+		return this.writefile(`${path}.ts`, tpl);
 	}
 
 	/**
@@ -104,6 +109,8 @@ const logger = anylogger('${vname}');
 
 export type ${rname} = {
 	ok: true;
+	data:{
+	};
 } | {
 	ok: false;
 	message: string;
@@ -123,7 +130,10 @@ handler.post(async(req, res) => {
 		logger.debug('msg body:', req.body);
 		const { } = req.body as ${mname};
 
-		res.status(200).json({ ok: true });
+		res.status(200).json({
+			ok: true,
+			data:{}
+		});
 	} catch (error) {
 		logger.error(error);
 		logger.trace(error);
